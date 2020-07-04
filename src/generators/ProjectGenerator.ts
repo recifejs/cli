a310925fa5f2fcff9eca394326b712230ebba129
André Lins <andrelucas01@hotmail.com>
2020-07-04 19:45:24 -0300

    Inserting inquirer | Create option packageManager

--- src/generators/ProjectGenerator.ts
+++ src/generators/ProjectGenerator.ts
@@ -1,50 +1,75 @@
 import fs from 'fs';
 import path from 'path';
 import commander from 'commander';
-import copyFolder from '../utils/copyFolder';
+import inquirer from 'inquirer';
 
+import copyFolder from '../utils/copyFolder';
 import createPackageJson from '../stages/CreatePackageJson';
 import installDependencies from '../stages/InstallDependencies';
 import initializeGit from '../stages/InitializeGit';
 import Log from '../Log';
 
-const createProject = (name: string) => {
-  if (name) {
-    Log.Instance.infoHeap(`Creating the project`);
+const createProject = (projectName: string, packageManager: 'yarn' | 'npm') => {
+  Log.Instance.infoHeap(`Creating the project`);
 
-    const source = path.join(__dirname, '/../../templates/project');
-    const target = path.join(process.cwd(), name);
+  const source = path.join(__dirname, '/../../templates/project');
+  const target = path.join(process.cwd(), projectName);
 
-    try {
-      Log.Instance.infoHeap(`Copying files`);
-      fs.mkdirSync(name);
+  try {
+    Log.Instance.infoHeap(`Copying files`);
+    fs.mkdirSync(projectName);
 
-      copyFolder(source, target);
-      fs.renameSync(
-        path.join(target, 'gitignore'),
-        path.join(target, '.gitignore')
-      );
-
-      createPackageJson(target, name);
-      installDependencies(target);
-      initializeGit(name);
-
-      Log.Instance.successHeap(`The ${name} project was created.`);
-      Log.Instance.info(`Path: ${target}\n\n`);
-    } catch (err) {
-      Log.Instance.exception(err);
-    }
-  } else {
-    Log.Instance.errorHeap(`Specify the name project.`);
-    Log.Instance.info(
-      `For example: recife-cli project my-project-name\nRun --help for more information`
+    copyFolder(source, target);
+    fs.renameSync(
+      path.join(target, 'gitignore'),
+      path.join(target, '.gitignore')
     );
+
+    createPackageJson(target, projectName);
+    installDependencies(target, packageManager);
+    initializeGit(projectName);
+
+    Log.Instance.successHeap(`The ${projectName} project was created.`);
+    Log.Instance.info(`Path: ${target}\n\n`);
+  } catch (err) {
+    Log.Instance.exception(err);
   }
+};
+
+const createProjectWithOptions = () => {
+  inquirer
+    .prompt([
+      {
+        name: 'projectName',
+        message: 'Project Name:',
+        type: 'string',
+        default: 'my-project'
+      },
+      {
+        name: 'packageManager',
+        message: 'Package Manager:',
+        type: 'list',
+        default: 'npm',
+        choices: ['npm', 'yarn']
+      }
+    ])
+    .then((answers: any) => {
+      Log.Instance.jump();
+      createProject(answers.projectName, answers.packageManager);
+    });
 };
 
 commander
   .name(`recife-cli project`)
-  .arguments('<project-name>')
-  .action(name => createProject(name))
+  .arguments('[project-name]')
+  .option('-p, --package-manager <packageManager>', 'Package Manager')
+  .action((name, cmd) => {
+    console.log(cmd);
+    if (name) {
+      createProject(name, cmd.packageManager || 'npm');
+    } else {
+      createProjectWithOptions();
+    }
+  })
   .allowUnknownOption(false)
   .parse(process.argv);

5d02492464aad4ffd323c8f4df63199e9e1d01a9
André Lins <andrelucas01@hotmail.com>
2020-07-04 21:07:07 -0300

    Removing browserList

--- src/stages/CreatePackageJson.ts
+++ src/stages/CreatePackageJson.ts
@@ -27,14 +27,6 @@ const createPackageJson = (
     dependencies: {
       recife: '^0.7.0',
       typescript: '^3.*'
-    },
-    browserslist: {
-      production: ['>0.2%', 'not dead', 'not op_mini all'],
-      development: [
-        'last 1 chrome version',
-        'last 1 firefox version',
-        'last 1 safari version'
-      ]
     }
   };
 
